80fd50d931eaf20496c6c3388dac4b0d3a8054ee
Renan <renandiutida@gmail.com>
2026-03-23 13:34:37 -0300

    Adicionando primeiro WebMethod, que é o de listar todos os equipamentos

--- src/main/java/br/com/fiap/soap/service/ReservaService.java
+++ src/main/java/br/com/fiap/soap/service/ReservaService.java
@@ -1,4 +1,24 @@
 package br.com.fiap.soap.service;
 
+import br.com.fiap.soap.model.Equipamento;
+import br.com.fiap.soap.model.Reserva;
+import br.com.fiap.soap.repository.ReservaRepository;
+
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebService;
+import java.util.List;
+
+@WebService
 public class ReservaService {
+
+    private final ReservaRepository repository = new ReservaRepository();
+
+    // Operação 1: listar todos os equipamentos disponíveis
+    @WebMethod
+    public List<Equipamento> listarEquipamentos() {
+        return repository.listarEquipamentos();
+    }
+
+
 }
