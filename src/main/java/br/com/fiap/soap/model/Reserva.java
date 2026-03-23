93bb29eb406d4dc9399ea89fda2664ab7645c383
Renan <renandiutida@gmail.com>
2026-03-23 13:19:10 -0300

    Reserva.java: Adicionando os atributos, construtores e métodos get/set

--- src/main/java/br/com/fiap/soap/model/Reserva.java
+++ src/main/java/br/com/fiap/soap/model/Reserva.java
@@ -1,4 +1,53 @@
 package br.com.fiap.soap.model;
 
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
 public class Reserva {
+
+    private int id;
+    private int equipamentoId;
+    private String responsavel;
+    private String status; // "ATIVA" ou "CANCELADA"
+
+    public Reserva() {}
+
+    public Reserva(int id, int equipamentoId, String responsavel, String status) {
+        this.id = id;
+        this.equipamentoId = equipamentoId;
+        this.responsavel = responsavel;
+        this.status = status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getEquipamentoId() {
+        return equipamentoId;
+    }
+
+    public void setEquipamentoId(int equipamentoId) {
+        this.equipamentoId = equipamentoId;
+    }
+
+    public String getResponsavel() {
+        return responsavel;
+    }
+
+    public void setResponsavel(String responsavel) {
+        this.responsavel = responsavel;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
