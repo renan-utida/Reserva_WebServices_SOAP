c2f853c6f42cee917a7f0a79f6105877d826dc44
Renan <renandiutida@gmail.com>
2026-03-23 13:23:10 -0300

    Equipamento.java: Adicionando os atributos, construtores e métodos get/set

--- src/main/java/br/com/fiap/soap/model/Equipamento.java
+++ src/main/java/br/com/fiap/soap/model/Equipamento.java
@@ -1,4 +1,53 @@
 package br.com.fiap.soap.model;
 
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
 public class Equipamento {
+
+    private int id;
+    private String nome;
+    private String descricao;
+    private boolean disponivel;
+
+    public Equipamento() {}
+
+    public Equipamento(int id, String nome, String descricao, boolean disponivel) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.disponivel = disponivel;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public boolean isDisponivel() {
+        return disponivel;
+    }
+
+    public void setDisponivel(boolean disponivel) {
+        this.disponivel = disponivel;
+    }
 }
